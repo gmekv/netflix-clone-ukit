233dd1c1bf8a12f5680d6e263e4f70ca1b182785
Giorgi <giorgi.mekvabishvili@protonmail.com>
2026-02-02 18:19:37 +0400

    Initial Commit

---
+++ Netflix Clone Ukit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone Ukit
+//
+//  Created by Giorgi Mekvabishvili on 02.02.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Netflix Clone UkitTests/Netflix_Clone_UkitTests.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_Clone_UkitTests.swift
+//  Netflix Clone UkitTests
+//
+//  Created by Giorgi Mekvabishvili on 02.02.26.
+//
+
+import Testing
+@testable import Netflix_Clone_Ukit
+
+struct Netflix_Clone_UkitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
