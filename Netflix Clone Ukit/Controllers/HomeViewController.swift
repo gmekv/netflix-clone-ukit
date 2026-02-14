a50462534c635dd8218a7a99d7fdb5ea0b1b487d
Giorgi <giorgi.mekvabishvili@protonmail.com>
2026-02-14 15:10:34 +0400

    Reduce navbar button sizes

--- Netflix Clone Ukit/Controllers/HomeViewController.swift
+++ Netflix Clone Ukit/Controllers/HomeViewController.swift
@@ -9,6 +9,39 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    private let logoButton: UIButton = {
+        let button = UIButton(type: .custom)
+        let image = UIImage(named: "netflixLogo")?.withRenderingMode(.alwaysOriginal)
+        button.setImage(image, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.frame = CGRect(x: 0, y: 0, width: 10, height: 10)
+        return button
+    }()
+    
+    private let personButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "person"), for: .normal)
+        button.tintColor = .white
+        button.setPreferredSymbolConfiguration(
+            UIImage.SymbolConfiguration(pointSize: 18, weight: .regular),
+            forImageIn: .normal
+        )
+        button.frame = CGRect(x: 0, y: 0, width: 26, height: 26)
+        return button
+    }()
+    
+    private let playButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "play.rectangle"), for: .normal)
+        button.tintColor = .white
+        button.setPreferredSymbolConfiguration(
+            UIImage.SymbolConfiguration(pointSize: 18, weight: .regular),
+            forImageIn: .normal
+        )
+        button.frame = CGRect(x: 0, y: 0, width: 26, height: 26)
+        return button
+    }()
+    
     private let homeFeedTable: UITableView = {
         let table = UITableView()
         table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
@@ -30,22 +63,7 @@ class HomeViewController: UIViewController {
     }
     
     private func configureNavbar() {
-        let logoImage = UIImage(named: "netflixLogo")?.withRenderingMode(.alwaysOriginal)
-        let logoButton = UIButton(type: .custom)
-        logoButton.setImage(logoImage, for: .normal)
-        logoButton.frame = CGRect(x: 0, y: 0, width: 28, height: 28) // adjust size
-        logoButton.imageView?.contentMode = .scaleAspectFit
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: logoButton)
-
-        let personButton = UIButton(type: .system)
-        personButton.setImage(UIImage(systemName: "person"), for: .normal)
-        personButton.tintColor = .white
- 
-
-        let playButton = UIButton(type: .system)
-        playButton.setImage(UIImage(systemName: "play.rectangle"), for: .normal)
-        playButton.tintColor = .white
-      
         navigationItem.rightBarButtonItems = [
             UIBarButtonItem(customView: personButton),
             UIBarButtonItem(customView: playButton)
@@ -91,5 +109,12 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 40
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defaultOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defaultOffset
+        
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
     }
 }
